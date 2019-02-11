eb4210721a92f8f14a220b04db1364ea861361a7
darian <873246925@qq.com>
2019-02-11 16:10:29 +0800

    't_up'

---
+++ Config.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ty_Ro
+ * Date: 2019/2/11
+ * Time: 16:09
+ */
+return [
+    'MYSQL' => [
+        'host'          => '192.168.75.1',
+        'port'          => '3306',
+        'user'          => 'root',
+        'timeout'       => '5',
+        'charset'       => 'utf8mb4',
+        'password'      => 'root',
+        'database'      => 'cry',
+        'POOL_MAX_NUM'  => '20',
+        'POOL_TIME_OUT' => '0.1',
+    ]
+];
