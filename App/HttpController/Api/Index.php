09533840976f280b706d708a09126724cf9d85ea
darian <873246925@qq.com>
2019-02-11 16:13:15 +0800

    't_up'

--- App/HttpController/Api/Index.php
+++ App/HttpController/Api/Index.php
@@ -15,7 +15,8 @@ class Index extends Base
 {
     public function video(){
 
-        $conf = new \EasySwoole\Mysqli\Config(\EasySwoole\EasySwoole\Config::getInstance()->getConf('MYSQL'));
+        $conf = new \EasySwoole\Mysqli\Config(\EasySwoole\EasySwoole\Config::getInstance()->getConf(''));
+        return $conf;
         $db = new Mysqli($conf);
         $data = $db->get('test');//获取一个表的数据
         $res = [
