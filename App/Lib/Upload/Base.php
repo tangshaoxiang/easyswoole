5b37d6ee80fa06ca005497656abd0ed8df0158cd
darian <873246925@qq.com>
2019-02-13 16:19:58 +0800

    't_up'

--- App/Lib/Upload/Base.php
+++ App/Lib/Upload/Base.php
@@ -8,12 +8,40 @@
 namespace App\Lib\Upload;
 
 class Base{
+
+    public $type = "";
+
     public function __construct($request)
     {
-        print_r($request);
+        $this->request = $request;
+        $files = $this->request->getSwooleRequest()->files;
+        $type = array_keys($files);
+        $this->type = $type[0];
+        print_r($files);
     }
 
     public function upload(){
+        if ($this->type != $this->fileType) {
+            return false;
+        }
 
+        $videos = $this->request->getUploadedFile($this->type);
+
+        $this->size = $videos->getSize();
+
+        $this->checkSize();
+
+        $fileName = $videos->getClientFileName();
+
+        $this->clientMediaType = $videos->getClientMediaType();
+
+        print_r($fileName);
+        print_r($this->clientMediaType);
+    }
+
+    public function checkSize() {
+        if (empty($this->size)) {
+            return false;
+        }
     }
 }
--- App/Lib/Upload/Video.php
+++ App/Lib/Upload/Video.php
@@ -11,4 +11,8 @@ namespace App\Lib\Upload;
 
 class Video extends Base {
 
+    public $fileType = "video";
+
+    public $maxSize = 122;
+
 }
